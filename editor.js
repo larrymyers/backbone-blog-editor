34f6ec3e14d408cdad4b91b41e3482107b087952
Larry Myers <larry@larrymyers.com>
2011-10-26 23:03:16 -0400

    initial import

---
+++ editor.js
@@ -0,0 +1,19 @@
+(function() {
+    var Article = Backbone.Model.extend({
+        
+    });
+    
+    var ActionsView = Backbone.View.extend({
+        
+    });
+    
+    var ArticleView = Backbone.View.extend({
+        
+    });
+    
+    var Editor = Backbone.View.extend({
+        initialize: function(options) {
+            
+        }
+    });
+})();
---
+++ main.js
@@ -0,0 +1,5 @@
+require(['jquery', 'underscore', 'backbone', 'backbone.localStorage', 'editor'], function() {
+    $(document).ready(function() {
+        Backbone.localStorageDB = new Store('editor');
+    });
+});
