a7bdc4b1e1b187d96ffd2c87840ff358f0cba345
Larry Myers <larry@larrymyers.com>
2011-11-04 13:41:40 -0400

    reshuffle source folders

---
+++ js/preview.js
@@ -0,0 +1,47 @@
+var MarkdownPreview = Backbone.View.extend({
+        
+    events: {
+        'click .modalCloseBtn': 'destroy'
+    },
+    
+    initialize: function(options) {
+        var self = this;
+        
+        self.shadow = $(self.make('div', { class: 'shadow' }));
+        self.shadow.click(_.bind(self.destroy, self));
+        self.closeBtn = $(self.make('img', { class: 'modalCloseBtn', src: 'img/close.png' }));
+        
+        $(self.el).addClass('modal').append(self.closeBtn);
+    },
+    
+    render: function() {
+        var converter = new Showdown.converter(),
+            htmlContent = converter.makeHtml(this.model.get('content')),
+            height = $(window).height(),
+            width = $(window).width();
+        
+        this.shadow.height(height).width(width);
+        
+        var content = this.make('div', { class: 'modalContent' });
+        
+        $(content).append('<h1>' + this.model.get('title') + '</h1>');
+        $(content).append(htmlContent);
+        $(this.el).append(content);
+        
+        $(this.el).width(Math.min(width*0.8, 600));
+        var centerWidth = width/2 - $(this.el).width()/2;
+        
+        $(this.el).css('left', centerWidth + 'px').height(height - 90);
+        $(content).height(height - 90);
+        
+        $('body').append(this.shadow);
+        $('body').append(this.el);
+    },
+    
+    destroy: function() {
+        // TODO remove shadow click handler
+        this.shadow.remove();
+        this.remove();
+    }
+    
+});
