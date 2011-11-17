7c7ccda29dbf5cb8c74223c2aac1344c1990136f
Larry Myers <larry@larrymyers.com>
2011-11-17 10:41:55 -0500

    update to jquery 1.7

--- test/previewSpec.js
+++ test/previewSpec.js
@@ -1,5 +1,7 @@
 describe('MarkdownPreview', function() {
     it('Should be initialized with a model containing title and content.', function() {
         var view = new MarkdownPreview({ content: 'Test' });
+        
+        expect(true).toBeTruthy();
     });
 });
