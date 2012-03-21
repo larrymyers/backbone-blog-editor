1189e88e01afdb3ff33d491ceba44ab9f6ebdfae
Larry Myers <larry@larrymyers.com>
2012-03-21 19:15:44 -0400

    add rjs optimizer build profile

---
+++ app.build.js
@@ -0,0 +1,12 @@
+({
+    baseUrl: "js",
+    name: "main",
+    out: "js/app.js",
+    paths: {
+        jquery: "empty:",
+        backbone: "ext/backbone",
+        "backbone.localStorage": "ext/backbone.localStorage",
+        underscore: "ext/underscore",
+        showdown: "ext/showdown"
+    }
+})
